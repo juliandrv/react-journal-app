ee31a6075d3c2e87016abd0a0ed8c0fbb9ef20a6
Julián Ramírez <judramirezva@gmail.com>
2025-04-10 20:23:20 -0500

    Fixed fileUpload

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,9 +1,8 @@
-// helpers/fileUpload.js
-
 export const fileUpload = async (file) => {
   const cloudUrl = 'https://api.cloudinary.com/v1_1/juliandrv/upload';
 
   const formData = new FormData();
+
   formData.append('upload_preset', 'react-journal');
   formData.append('file', file);
 
@@ -17,12 +16,10 @@ export const fileUpload = async (file) => {
 
     const cloudResp = await resp.json();
 
-    return {
-      secure_url: cloudResp.secure_url,
-      public_id: cloudResp.public_id,
-    };
+    return cloudResp.secure_url;
   } catch (error) {
-    console.error('Error uploading image', error);
+    console.log(error);
+
     return null;
   }
 };
